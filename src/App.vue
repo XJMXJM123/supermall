408a96a98ec228f5c99fb116fcf79b10925a4219
XJMXJM123 <2820794579@qq.com>
2022-07-31 14:44:07 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,15 @@
 <script>
 
 
-export default {
-  name: 'app',
-  components: {
+  export default {
+    name: 'app',
+    components: {
 
+    }
   }
-}
 </script>
 
 <style>
-
+  @import "./assets/css/base.css";
 </style>
+
